3839bad234057f73d3bf689cb6b6c223276c417f
MaksymBratsiun <bratsyun_m@ukr.net>
2023-04-03 14:27:52 +0300

    add CRUD

--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -19,10 +19,43 @@ async def get_contacts(db: Session = Depends(get_db)):
 @router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
 async def create_contact(body: ContactModel, db: Session = Depends(get_db)):
     contact = db.query(Contact).filter_by(email=body.email).first()
-    if contact is not None:
+    if contact:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email is exists")
     contact = Contact(**body.dict())
     db.add(contact)
     db.commit()
     db.refresh(contact)
+    return contact
+
+
+@router.get("/{contact_id}", response_model=ContactResponse, name="Get contact by id")
+async def get_contact_by_id(contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter_by(id=contact_id).first()
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+    return contact
+
+
+@router.put("/{contact_id}", response_model=ContactResponse, name="Update contact by id")
+async def update_contact_by_id(body: ContactModel, contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter_by(id=contact_id).first()
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+    contact.first_name = body.first_name
+    contact.last_name = body.last_name
+    contact.email = body.email
+    contact.phone = body.phone
+    contact.born_date = body.born_date
+    contact.add_data = body.add_data
+    db.commit()
+    return contact
+
+
+@router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT, name="Remove contact by id")
+async def remove_contact_by_id(contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter_by(id=contact_id).first()
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+    db.delete(contact)
+    db.commit()
     return contact
